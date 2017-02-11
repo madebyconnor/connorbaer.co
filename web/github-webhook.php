0571917c2b5df9b77a8e9f24e160f7624ff66956
Connor Bär <hello@connorbaer.io>
2017-02-11 21:41:30 +0100

    Adds back GitHub webhook.

---
+++ web/github-webhook.php
@@ -0,0 +1,11 @@
+<?php
+ignore_user_abort( true );
+set_time_limit( 0 );
+$repo          = '/srv/users/serverpilot/apps/connorbaer';
+$branch        = 'master';
+$output        = array();
+// Update the repo.
+$output[] = date( 'Y-m-d, H:i:s', time() ) . "\n";
+$output[] = "GitHub Pull\n============================\n" . shell_exec( 'cd ' . $repo . ' && git pull origin ' . $branch );
+// Redirect output to logs.
+file_put_contents( rtrim( getcwd(), '/' ) . '/github-log.txt', implode( "\n", $output ) . "\n----------------------------\n", FILE_APPEND );
