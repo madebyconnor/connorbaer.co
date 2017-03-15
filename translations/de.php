f6450bc4652b5622650512a910b74e19d48681bc
Connor Bär <hello@connorbaer.io>
2017-03-15 11:02:40 +0100

    German translations.

--- translations/de.php
+++ translations/de.php
@@ -3,4 +3,8 @@
 return array(
     "Menu" => "Menü",
     "Close" => "Schließen",
+    "All rights reserved." => "Alle Rechte vorbehalten",
+    "Email" => "E-Mail",
+    "Tel" => "Tel",
+    "Legal" => "Impressum",
 );
